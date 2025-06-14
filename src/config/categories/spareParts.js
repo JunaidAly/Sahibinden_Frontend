9710c232a4154ad8b5d2c1480df0ee7a401027d8
JunaidAly <juanidalikhan03@gmail.com>
2025-06-14 15:31:02 +0500

    spare parts categories complete

--- src/config/categories/spareParts.js
+++ src/config/categories/spareParts.js
@@ -66,10 +66,10 @@ export const sparePartsConfig = {
   { id: "orientation", label: "Orientation" }
 ],
 "accessories_tuning": [
-  { id: "cars_suvs", label: "Cars & SUVs" },
-  { id: "minivan_panel_van", label: "Minivan & Panel Van" },
-  { id: "commercial_vehicles", label: "Commercial Vehicles" },
-  { id: "caravan", label: "Caravan" }
+  { id: "cars_suvs_tuning", label: "Cars & SUVs" },
+  { id: "minivan_panel_van_tuning", label: "Minivan & Panel Van" },
+  { id: "commercial_vehicles_tuning", label: "Commercial Vehicles" },
+  { id: "caravan_tuning", label: "Caravan" }
 ],
 "rims_tires": [
   { id: "rim", label: "Rim" },
@@ -116,6 +116,142 @@ export const sparePartsConfig = {
 "marine_engine": [
   { id: "outboard", label: "Outboard" },
   { id: "inboard", label: "Inboard" }
+]
+  },
+  level4:{
+    "cars_suvs": [
+  { id: "ignition_fuel", label: "Ignition & Fuel" },
+  { id: "exhaust", label: "Exhaust" },
+  { id: "electric", label: "Electric" },
+  { id: "filter", label: "Filter" },
+  { id: "brake_clutch", label: "Brake & Clutch" },
+  { id: "heating_ventilation_air", label: "Heating & Ventilation & Air" },
+  { id: "body_bodywork", label: "Body & Bodywork" },
+  { id: "mechanical", label: "Mechanical" },
+  { id: "engine", label: "Engine" },
+  { id: "transmission_gear", label: "Transmission & Gear" },
+  { id: "walking_steering", label: "Walking & Steering" }
+],
+"minivan_panel_van": [
+  { id: "ignition_fuel", label: "Ignition & Fuel" },
+  { id: "exhaust", label: "Exhaust" },
+  { id: "electric", label: "Electric" },
+  { id: "filter", label: "Filter" },
+  { id: "brake_clutch", label: "Brake & Clutch" },
+  { id: "heating_ventilation_air", label: "Heating & Ventilation & Air" },
+  { id: "body_bodywork", label: "Body & Bodywork" },
+  { id: "mechanical", label: "Mechanical" },
+  { id: "engine", label: "Engine" },
+  { id: "transmission_gear", label: "Transmission & Gear" },
+  { id: "walking_steering", label: "Walking & Steering" }
+],
+"commercial_vehicles": [
+  { id: "minibus_bus", label: "Minibus & Bus" },
+  { id: "truck_tractor", label: "Truck & Tractor" },
+  { id: "truck_pickup", label: "Truck & Pickup" },
+  { id: "car_rescue_transporter", label: "Car Rescue & Transporter" },
+  { id: "trailer", label: "Trailer" }
+],
+"cars_suvs_tuning": [
+  { id: "interior_accessories", label: "Interior Accessories" },
+  { id: "exterior_accessories", label: "Exterior Accessories" },
+  { id: "performance", label: "Performance" },
+  { id: "electronic", label: "Electronic" },
+  { id: "maintenance_repair_products", label: "Maintenance & Repair Products" }
+],
+"minivan_panel_van_tuning": [
+  { id: "interior_accessories", label: "Interior Accessories" },
+  { id: "exterior_accessories", label: "Exterior Accessories" },
+  { id: "maintenance_repair_products", label: "Maintenance & Repair Products" }
+],
+"commercial_vehicles_tuning": [
+  { id: "minibus_bus", label: "Minibus & Bus" },
+  { id: "truck_tractor", label: "Truck & Tractor" },
+  { id: "truck_pickup", label: "Truck & Pickup" }
+],
+"accessory": [
+  { id: "cd_changer", label: "CD Changer" },
+  { id: "crossover", label: "Crossover" },
+  { id: "deck_converter", label: "Deck Converter" },
+  { id: "equalizer", label: "Equalizer" },
+  { id: "fm_transmitter", label: "FM Transmitter" },
+  { id: "speaker_cover", label: "Speaker Cover" },
+  { id: "speaker_panfoot", label: "Speaker Panfoot" },
+  { id: "inverter", label: "Inverter" },
+  { id: "cabin", label: "Cabin" },
+  { id: "cable", label: "Cable" },
+  { id: "capacitor", label: "Capacitor" },
+  { id: "cassette_adapter", label: "Cassette Adapter" },
+  { id: "sound_processor", label: "Sound Processor" },
+  { id: "insurance", label: "Insurance" },
+  { id: "tape_frame", label: "Tape Frame" },
+  { id: "remote_control", label: "Remote control" },
+  { id: "insulation_materials", label: "Insulation Materials" }
+],
+"navigation_device": [
+  { id: "alpine", label: "Alpine" },
+  { id: "aselsan", label: "Aselsan" },
+  { id: "becker", label: "Becker" },
+  { id: "blaupunkt", label: "Blaupunkt" },
+  { id: "nightingale", label: "Nightingale" },
+  { id: "carnavi", label: "Carnavi" },
+  { id: "cartel", label: "Cartel" },
+  { id: "columbus", label: "Columbus" },
+  { id: "cohwegen", label: "Cohwegen" },
+  { id: "corvin", label: "Corvin" },
+  { id: "ducar", label: "Ducar" },
+  { id: "du_iron_master", label: "Du Iron Master" },
+  { id: "falk", label: "Falk" },
+  { id: "fthsound", label: "Fthsound" },
+  { id: "garmin", label: "Garmin" },
+  { id: "goaudio", label: "GoAudio" },
+  { id: "goldmaster", label: "GoldMaster" },
+  { id: "gpsturk", label: "GPSTurk" },
+  { id: "harvard", label: "Harvard" },
+  { id: "hp", label: "HP" },
+  { id: "jameson", label: "Jameson" },
+  { id: "kowloon", label: "Kowloon" },
+  { id: "magellan", label: "Magellan" },
+  { id: "medion", label: "Medion" },
+  { id: "mio", label: "MIO" },
+  { id: "mixtech", label: "Mixtech" },
+  { id: "motevo", label: "Motevo" },
+  { id: "mustek", label: "Mustek" },
+  { id: "myguide", label: "MyGuide" },
+  { id: "navia", label: "Navia" },
+  { id: "navibose", label: "Navibose" },
+  { id: "navigold", label: "Navigold" },
+  { id: "navigon", label: "Navigon" },
+  { id: "navimedia", label: "Navimedia" },
+  { id: "navirex", label: "Navirex" },
+  { id: "navitech", label: "Navitech" },
+  { id: "navix", label: "Navix" },
+  { id: "navking", label: "Navking" },
+  { id: "navsonic", label: "Navsonic" },
+  { id: "necvox", label: "Necvox" },
+  { id: "new_space", label: "New Space" },
+  { id: "next", label: "Next" },
+  { id: "oblio", label: "Oblio" },
+  { id: "pioneer", label: "Pioneer" },
+  { id: "piranha", label: "Piranha" },
+  { id: "remco", label: "Remco" },
+  { id: "roadstar", label: "Roadstar" },
+  { id: "sunny", label: "Sunny" },
+  { id: "telefunken", label: "Telefunken" },
+  { id: "tomtom", label: "TomTom" },
+  { id: "vendeka", label: "Vendeka" },
+  { id: "viamichelin", label: "ViaMichelin" },
+  { id: "xetec", label: "Xetec" },
+  { id: "yakumo", label: "Yakumo" },
+  { id: "other", label: "Other" }
+],
+"speaker": [
+  { id: "coaxial_coax", label: "Coaxial (Coax)" },
+  { id: "component", label: "Component" },
+  { id: "midrange", label: "Midrange" },
+  { id: "subwoofer", label: "Subwoofer" },
+  { id: "twitter", label: "Twitter" },
+  { id: "woofer", label: "Woofer" }
 ]
   }
 };
